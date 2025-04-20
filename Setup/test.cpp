2d6de2098b11d308c014d9b970dbf51e6c2bc8a7
Ishrak-Adit07 <ishrak.adit07@gmail.com>
2025-04-20 22:49:08 +0600

    OpenGL setup & A1 spec commit

---
+++ Setup/test.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+    int number;
+    std::cout << "Enter a number: ";
+    std::cin >> number;
+
+    std::cout << "Number + 10 = " << (number + 10) << std::endl;
+    return 0;
+}
