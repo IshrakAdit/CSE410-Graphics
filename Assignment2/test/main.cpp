aa0a2cf5d88fc0db25093c0c64585620e736c0a9
IshrakAdit <ishrak.adit07@gmail.com>
2025-06-09 02:04:49 +0600

    A2 - clipping & scan conversion

--- Assignment2/test/main.cpp
+++ Assignment2/test/main.cpp
@@ -17,6 +17,7 @@ int main(void)
     ofstream stage1_stream("stage1.txt");
     ofstream stage2_stream("stage2.txt");
     ofstream stage3_stream("stage3.txt");
+    ofstream stage4_stream("z_buffer.txt");
 
     // Output precisions
     stage1_stream << fixed << setprecision(7);
@@ -153,8 +154,8 @@ int main(void)
 
         // Projection of triangle edges on xy plane ( z = 0 )
         Line projection_of_AB = Line(Point(A.x, A.y, 0), Point(B.x, B.y, 0));
-        Line projection_of_BC = Line(Point(B.x, B.y, 0), Point(C.x, C.y, 0));
         Line projection_of_AC = Line(Point(A.x, A.y, 0), Point(C.x, C.y, 0));
+        Line projection_of_BC = Line(Point(B.x, B.y, 0), Point(C.x, C.y, 0));
 
         double bottom_scanline = max(C.y, bottommost_center_y);
         double top_scanline = min(A.y, topmost_center_y);
@@ -168,17 +169,100 @@ int main(void)
 
             Line current_line = Line(Point(0, y_s, 0), Point(1, y_s, 0));
 
-            pair<bool, Point> l1_intersection =
-                check_line_segment_intersection(current_line, projection_of_AB);
-            pair<bool, Point> l2_intersection =
-                check_line_segment_intersection(current_line, projection_of_AC);
-            pair<bool, Point> l3_intersection =
-                check_line_segment_intersection(current_line, projection_of_BC);
-        }
+            pair<bool, Point> AB_line_intersection = check_line_segment_intersection(current_line, projection_of_AB);
+            pair<bool, Point> AC_line_intersection = check_line_segment_intersection(current_line, projection_of_AC);
+            pair<bool, Point> BC_line_intersection = check_line_segment_intersection(current_line, projection_of_BC);
 
-        // All file streams closed
-        scene_stream.close();
-        stage1_stream.close();
-        stage2_stream.close();
-        stage3_stream.close();
+            int intersection_count = AB_line_intersection.first + AC_line_intersection.first + BC_line_intersection.first;
+
+            auto reorder_points_and_lines = [&](string order)
+            {
+                if (order[1] > order[2])
+                    swap(order[1], order[2]);
+                if (order == "ABC")
+                {
+                    // No change
+                }
+                else if (order == "BAC")
+                {
+                    tie(A, B, C) = make_tuple(B, A, C);
+                    tie(projection_of_AB, projection_of_AC, projection_of_BC) =
+                        make_tuple(projection_of_AB, projection_of_BC, projection_of_AC);
+                    tie(AB_line_intersection, AC_line_intersection, BC_line_intersection) =
+                        make_tuple(AB_line_intersection, BC_line_intersection, AC_line_intersection);
+                }
+                else if (order == "CAB")
+                {
+                    tie(A, B, C) = make_tuple(C, A, B);
+                    tie(projection_of_AB, projection_of_AC, projection_of_BC) =
+                        make_tuple(projection_of_AC, projection_of_BC, projection_of_AB);
+                    tie(AB_line_intersection, AC_line_intersection, BC_line_intersection) =
+                        make_tuple(AC_line_intersection, BC_line_intersection, AB_line_intersection);
+                }
+            };
+
+            if (intersection_count == 0)
+                continue;
+            if (intersection_count == 2)
+            {
+                reorder_points_and_lines(AB_line_intersection.first
+                                             ? (AC_line_intersection.first ? "ABC" : "BAC")
+                                             : "CAB");
+            }
+
+            double x_a = AB_line_intersection.second.x;
+            double x_b = AC_line_intersection.second.x;
+            double z_a = A.z - (A.z - B.z) * (A.y - y_s) / (A.y - B.y);
+            double z_b = A.z - (A.z - C.z) * (A.y - y_s) / (A.y - C.y);
+
+            if (x_a > x_b)
+            {
+                swap(x_a, x_b);
+                swap(z_a, z_b);
+            }
+
+            int left_col = round((max(x_a, leftmost_center_x) - leftmost_center_x) / pixel_width);
+            int right_col = round((min(x_b, rightmost_center_x) - leftmost_center_x) / pixel_width);
+
+            for (int j = left_col; j <= right_col; j++)
+            {
+                double x_p = leftmost_center_x + j * pixel_width;
+                double z_p = z_b - (z_b - z_a) * (x_b - x_p) / (x_b - x_a);
+
+                if (z_p >= z_min && z_p < z_buffer[screen_height - 1 - i][j])
+                {
+                    z_buffer[screen_height - 1 - i][j] = z_p;
+                    image.set_pixel(j, screen_height - 1 - i, triangle.red,
+                                    triangle.green, triangle.blue);
+                }
+            }
+        }
     }
+
+    image.save_image("out.bmp");
+
+    for (int i = 0; i < screen_height; i++)
+    {
+        for (int j = 0; j < screen_width; j++)
+        {
+            if (z_buffer[i][j] >= z_max)
+                continue;
+            stage4_stream << z_buffer[i][j] << "\t";
+        }
+        stage4_stream << endl;
+    }
+
+    image.clear();
+    z_buffer.clear();
+
+    // All file streams closed
+    scene_stream.close();
+    config_stream.close();
+
+    stage1_stream.close();
+    stage2_stream.close();
+    stage3_stream.close();
+    stage4_stream.close();
+
+    return 0;
+}
